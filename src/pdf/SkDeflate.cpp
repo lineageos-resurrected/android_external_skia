5e587be246f10faf9490d76850b1430422a67770
Ben Wagner <bungeman@google.com>
2024-08-12 15:00:08 -0400

    Bounds check in skia_alloc_func
    The allocator callback for zlib needs to check that items * size will
    fit in size_t and return nullptr if not.
    
    Conflicts:
    - src/pdf/SkDeflate.cpp: just in header includes
    
    Bug: 349678452
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/888996
    Commit-Queue: Ben Wagner <bungeman@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:94b46e52960ec84a69304ea058fd928e3de6fa56)
    Merged-In: Id1a30592d435bd0de4630e7047f26b0dc17654fc
    Change-Id: Id1a30592d435bd0de4630e7047f26b0dc17654fc
    
    Change-Id: I1c198cb3586db94b45d6f6592fac9c0dc9f19f23

--- src/pdf/SkDeflate.cpp
+++ src/pdf/SkDeflate.cpp
@@ -10,6 +10,7 @@
 #include "SkData.h"
 #include "SkDeflate.h"
 #include "SkStream.h"
+#include "SkTFitsIn.h"
 
 #ifdef ZLIB_INCLUDE
     #include ZLIB_INCLUDE
@@ -22,6 +23,13 @@ namespace {
 // Different zlib implementations use different T.
 // We've seen size_t and unsigned.
 template <typename T> void* skia_alloc_func(void*, T items, T size) {
+    if (!SkTFitsIn<size_t>(size)) {
+        return nullptr;
+    }
+    const size_t maxItems = SIZE_MAX / size;
+    if (maxItems < items) {
+        return nullptr;
+    }
     return sk_calloc_throw(SkToSizeT(items) * SkToSizeT(size));
 }
 
